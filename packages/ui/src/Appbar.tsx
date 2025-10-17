5b5fc437824f9c167513869954152574905ec61a
pranayburra <125851398+pranayburra@users.noreply.github.com>
2025-10-17 13:02:40 +0530

    changed_Appbar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -19,7 +19,7 @@ export const Appbar = ({
             PayTM
         </div>
         <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+            <Button disable={false} onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
     </div>
 }
